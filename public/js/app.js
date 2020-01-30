cb93414def5af80b94c920f19415e947af0b939f
Allen Cohen <azc666@gmail.com>
2020-01-29 21:49:31 -0500

    responsive footer

--- public/js/app.js
+++ public/js/app.js
@@ -2056,33 +2056,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -33076,7 +33049,110 @@ var render = function() {
     "footer",
     { staticClass: "absolute w-full bottom-0 bg-teal-400 opacity-75" },
     [
-      _vm._m(0),
+      _c(
+        "div",
+        {
+          staticClass: "sm:flex items-center justify-around px-4 py-2 text-sm"
+        },
+        [
+          _vm._m(0),
+          _vm._v(" "),
+          _c("hr", {
+            staticClass: "border border-gray-500 mt-2 mb-2 sm:hidden"
+          }),
+          _vm._v(" "),
+          _c("div", { staticClass: "text-center flex-1 text-gray-700" }, [
+            _c(
+              "svg",
+              {
+                staticClass: "h-4 w-4 inline-block",
+                attrs: {
+                  xmlns: "http://www.w3.org/2000/svg",
+                  viewBox: "0 0 512 512"
+                }
+              },
+              [
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"
+                  }
+                })
+              ]
+            ),
+            _vm._v("\n  813-254-9444\n        "),
+            _c("br"),
+            _vm._v(" "),
+            _c(
+              "svg",
+              {
+                staticClass: "h-4 w-4 inline-block",
+                attrs: {
+                  xmlns: "http://www.w3.org/2000/svg",
+                  viewBox: "0 0 512 512"
+                }
+              },
+              [
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M128 144v320c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V144c0-26.51 21.49-48 48-48h32c26.51 0 48 21.49 48 48zm384 64v256c0 26.51-21.49 48-48 48H192c-26.51 0-48-21.49-48-48V40c0-22.091 17.909-40 40-40h207.432a39.996 39.996 0 0 1 28.284 11.716l48.569 48.569A39.999 39.999 0 0 1 480 88.568v74.174c18.641 6.591 32 24.36 32 45.258zm-320-16h240V96h-24c-13.203 0-24-10.797-24-24V48H192v144zm96 204c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40zm0-128c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40zm128 128c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40zm0-128c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40z"
+                  }
+                })
+              ]
+            ),
+            _vm._v("\n 813-254-9445\n        "),
+            _c("br"),
+            _vm._v(" "),
+            _c(
+              "svg",
+              {
+                staticClass: "h-4 w-4 inline-block",
+                attrs: {
+                  xmlns: "http://www.w3.org/2000/svg",
+                  viewBox: "0 0 512 512"
+                }
+              },
+              [
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M256 8C118.941 8 8 118.919 8 256c0 137.059 110.919 248 248 248 48.154 0 95.342-14.14 135.408-40.223 12.005-7.815 14.625-24.288 5.552-35.372l-10.177-12.433c-7.671-9.371-21.179-11.667-31.373-5.129C325.92 429.757 291.314 440 256 440c-101.458 0-184-82.542-184-184S154.542 72 256 72c100.139 0 184 57.619 184 160 0 38.786-21.093 79.742-58.17 83.693-17.349-.454-16.91-12.857-13.476-30.024l23.433-121.11C394.653 149.75 383.308 136 368.225 136h-44.981a13.518 13.518 0 0 0-13.432 11.993l-.01.092c-14.697-17.901-40.448-21.775-59.971-21.775-74.58 0-137.831 62.234-137.831 151.46 0 65.303 36.785 105.87 96 105.87 26.984 0 57.369-15.637 74.991-38.333 9.522 34.104 40.613 34.103 70.71 34.103C462.609 379.41 504 307.798 504 232 504 95.653 394.023 8 256 8zm-21.68 304.43c-22.249 0-36.07-15.623-36.07-40.771 0-44.993 30.779-72.729 58.63-72.729 22.292 0 35.601 15.241 35.601 40.77 0 45.061-33.875 72.73-58.161 72.73z"
+                  }
+                })
+              ]
+            ),
+            _vm._v("\n output@g-d.com\n        "),
+            _c("br"),
+            _vm._v(" "),
+            _c(
+              "svg",
+              {
+                staticClass: "h-4 w-4 inline-block",
+                attrs: {
+                  xmlns: "http://www.w3.org/2000/svg",
+                  viewBox: "0 0 512 512"
+                }
+              },
+              [
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM128 148c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40zm320 0c0 6.6-5.4 12-12 12H188c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h248c6.6 0 12 5.4 12 12v40z"
+                  }
+                })
+              ]
+            ),
+            _vm._v(" www.g-d.com\n      ")
+          ]),
+          _vm._v(" "),
+          _c("hr", {
+            staticClass: "border border-gray-500 mt-2 mb-2 sm:hidden"
+          }),
+          _vm._v(" "),
+          _vm._m(1)
+        ]
+      ),
       _vm._v(" "),
       _c("hr", { staticClass: "border border-gray-500" }),
       _vm._v(" "),
@@ -33084,15 +33160,15 @@ var render = function() {
         "div",
         {
           staticClass:
-            "flex justify-center align-center text-sm text-gray-300 py-2 mb-1"
+            "flex justify-center align-center text-sm text-gray-400 py-2 mb-1"
         },
         [
-          _vm._v("\n    Copyright © " + _vm._s(_vm.date) + "\n    "),
+          _vm._v("\n      Copyright © " + _vm._s(_vm.date) + "\n      "),
           _c(
             "a",
             {
               staticClass:
-                "text-gray-300 hover:text-gray-400 text-sm font-semibold",
+                "text-gray-400 hover:text-gray-300 text-sm font-semibold",
               attrs: { href: "https://www.g-d.com" }
             },
             [_vm._v("  Graphics+Design")]
@@ -33109,47 +33185,35 @@ var staticRenderFns = [
     var _c = _vm._self._c || _h
     return _c(
       "div",
-      { staticClass: "flex items-center justify-around px-4 py-2 text-sm" },
+      {
+        staticClass:
+          "text-center flex-1 text-gray-700 text-2xl text-semiboldleading-none"
+      },
       [
-        _c(
-          "div",
-          {
-            staticClass:
-              "text-center flex-1 text-gray-700 text-2xl text-semibold"
-          },
-          [
-            _vm._v("\n      Graphics+Design\n      "),
-            _c("br"),
-            _vm._v(" "),
-            _c("span", { staticClass: "italic text-xl" }, [
-              _vm._v('"Imaging at the Speed of Business"')
-            ])
-          ]
-        ),
-        _vm._v(" "),
-        _c("div", { staticClass: "text-center flex-1 text-gray-700" }, [
-          _vm._v("\n      reach us\n      "),
+        _c("p", { staticClass: "leading-tight" }, [
+          _vm._v("\n          Graphics+Design\n          "),
           _c("br"),
-          _vm._v("p: 813-254-9444\n      "),
-          _c("br"),
-          _vm._v("f: 813-254-9445\n      "),
-          _c("br"),
-          _vm._v("e: output@g-d.com\n      "),
-          _c("br"),
-          _vm._v("w: www.g-d.com\n    ")
-        ]),
-        _vm._v(" "),
-        _c("div", { staticClass: "text-center flex-1 text-gray-700" }, [
-          _vm._v("\n      Graphics+Design\n      "),
-          _c("br"),
-          _vm._v("A Division of AZC Enterprises, Inc.\n      "),
-          _c("br"),
-          _vm._v("1111 West Cass Street\n      "),
-          _c("br"),
-          _vm._v("Tampa, FL 33606\n    ")
+          _vm._v(" "),
+          _c("span", { staticClass: "italic text-xl line-height-none" }, [
+            _vm._v('"Imaging at the Speed of Business"')
+          ])
         ])
       ]
     )
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "text-center flex-1 text-gray-700" }, [
+      _vm._v("\n        Graphics+Design\n        "),
+      _c("br"),
+      _vm._v("A Division of AZC Enterprises, Inc.\n        "),
+      _c("br"),
+      _vm._v("1111 West Cass Street\n        "),
+      _c("br"),
+      _vm._v("Tampa, FL 33606\n      ")
+    ])
   }
 ]
 render._withStripped = true

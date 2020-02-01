d5082ca21d3b10b6514fcfb3fa4acb4a938ba242
Allen Cohen <azc666@gmail.com>
2020-02-01 18:16:25 -0500

    revert footer

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,15 +2,13 @@
 
 @section('content')
 
-<div class="flex-1">
-  <h1 class=" mt-3 sm:mt-6 text-4xl sm:text-6xl text-center font-myriad font-bold leading-tight">Graphics+Design</h1>
-  <h3 class="text-2xl sm:text-3xl text-center font-myriad italic">Imaging at the Speed of Business</h3>
+<h1 class=" mt-3 sm:mt-6 text-4xl sm:text-6xl text-center font-myriad font-bold leading-tight">Graphics+Design</h1>
+<h3 class="text-2xl sm:text-3xl text-center font-myriad italic">Imaging at the Speed of Business</h3>
 
-  <div class="p-8 mt-5 sm:mt-10 mx-auto container background-trans border rounded-lg shadow-lg">
+<div class="p-8 mt-5 sm:mt-10 mx-auto container background-trans border rounded-lg shadow-lg">
 
-    <img class="h-48 sm:h-64 w-full object-cover" src="/assets/G+D Building.png" alt="">
+  <img class="h-48 sm:h-64 w-full object-cover" src="/assets/G+D Building.png" alt="">
 
-  </div>
 </div>
 
 @endsection

e94c04e0c9ed03d87e49b207f3a28673ef826dc0
Allen Cohen <azc666@gmail.com>
2020-02-05 21:14:40 -0500

    nav mod

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,7 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
   <link rel="stylesheet" href="https://use.typekit.net/lln4snf.css">
   <link rel="stylesheet" href="/css/app.css">
-  <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+  {{-- <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
   <script>
     $(function(){
           $('a').each(function(){
@@ -19,7 +19,7 @@
               }
           });
       });
-  </script>
+  </script> --}}
 </head>
 
 <body class="relative m-0 pb-48 min-h-screen bg-fixed bg-no-repeat bg-center bg-cover"
@@ -34,6 +34,16 @@
   {{-- </div> --}}
 
   <script src="/js/app.js"></script>
+  <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script>
+    $(function(){
+            $('a').each(function(){
+                if ($(this).prop('href') == window.location.href) {
+                    $(this).addClass('active'); $(this).parents('li').addClass('active');
+                }
+            });
+        });
+  </script>
 
 </body>
 
